28a6664c5049445afb5426dcdb064f310b7ef5d1
ika0216 <40966618+ika0216@users.noreply.github.com>
2021-09-09 16:23:30 +0900

    drill 04 완성
    2020180013 김지선

---
+++ 04/모눈 그리기.py
@@ -0,0 +1,35 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-250,250)
+turtle.pendown()
+
+count =  6
+while (count > 0):
+    turtle.forward(500)
+    turtle.penup()
+    turtle.backward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.pendown()
+    count -= 1
+
+turtle.penup()
+turtle.goto(-250,250)
+turtle.right(90)
+turtle.pendown()
+
+count =  6
+while (count > 0):
+    turtle.forward(500)
+    turtle.penup()
+    turtle.backward(500)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.pendown()
+    count -= 1
+
+turtle.exitonclick()
+
